6c7e62bd41bf7d58b8f8c4b03168b00df1f69770
Anasselb <elboussailianass@gmail.com>
2024-02-12 21:07:03 +0100

    printing is done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	//program should  print the string below  with out using printf or puts and return 1
+	    fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	        return 1;
+}
+
