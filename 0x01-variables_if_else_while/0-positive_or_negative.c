ae0971d49d525c496b70892832367c853bb2cf93
Anasselb <elboussailianass@gmail.com>
2024-02-19 21:40:37 +0100

    number state is done

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  
+int n ;
+n = 0 ;
+
+if (n > 0)
+{
+    printf("%d is positive\n",n) ;
+}
+else if (n < 0)
+{
+    printf("%d is negative\n",n) ; 
+}
+else
+{
+    printf("%d is zero\n",n) ;
+}
+
+}
