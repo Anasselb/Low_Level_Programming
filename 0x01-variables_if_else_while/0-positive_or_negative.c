5a76123e82d156459e90ac028c7a4278f02f6948
Anasselb <elboussailianass@gmail.com>
2024-02-19 21:52:14 +0100

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+/*
+exercice  : positive_or_negative 
+
+The output of the program should be:
+The number, followed by
+if the number is greater than 0: is positive
+if the number is 0: is zero
+if the number is less than 0: is negative
+followed by a new line
+*/
+
 
 int main()
 {
