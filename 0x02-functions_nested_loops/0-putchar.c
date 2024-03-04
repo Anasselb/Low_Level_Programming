b1dd56091a6d6210ef178c67c81436ce786d2fef
Anasselb <elboussailianass@gmail.com>
2024-03-04 22:35:37 +0100

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "header.h"
 
+/*
+  Write a program that prints _putchar, followed by a new line.
+  The program should return 0
+*/
+
 int main(void)
 {
     char an[] = "_putchar";
