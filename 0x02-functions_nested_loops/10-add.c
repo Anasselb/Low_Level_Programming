a9655c568377c510c54ddeafe9a8e69245320a5f
Anasselb <elboussailianass@gmail.com>
2024-03-09 15:46:06 +0100

    function add is complited

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Write a function that adds two integers and returns the result.
+ * int add(int, int);
+ *
+ * Return: Always 0.
+ */
+
+int add(int x, int y)
+{
+    return x + y;
+}
