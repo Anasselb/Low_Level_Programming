d33bc423b10b8866b19e6b4892f81a4362c7d54e
Anasselb <elboussailianass@gmail.com>
2024-03-20 00:12:08 +0100

    checking if the argument passed is digit

---
+++ 0x04_C-More_functions_more_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*
+ * Write a function that checks for a digit (0 through 9).
+ * Prototype: int _isdigit(int c);
+ * Returns 1 if c is a digit
+ * Returns 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+    if (c >= '0' && c <= '9')
+    {
+        return (1);
+    }
+    return (0);
+}
