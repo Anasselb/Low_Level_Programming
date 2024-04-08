61acb312683e3eca0ee39282a755dcd4d237dc41
Anasselb <elboussailianass@gmail.com>
2024-04-09 01:01:52 +0200

    update

--- 0x04_C-More_functions_more_nested_loops/100-prime_factor.c
+++ 0x04_C-More_functions_more_nested_loops/100-prime_factor.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+/*
+The prime factors of 1231952 are 2, 2, 2, 2, 37 and 2081.
+Write a program that finds and prints the largest prime factor of the number 612852475143, followed by a new line.
+You are allowed to use the standard library
+Your program will be compiled with this command: gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-prime_factor.c -o 100-prime_factor -lm
+*/
+
 int main(void)
 {
 
     long int n = 612852475143, factors = 2, the_largest_one;
-    
+
     while (n != 1)
     {
     stepone:
