a56e56d47cc1a504eab2e7303d074b6a43d577dc
Anasselb <elboussailianass@gmail.com>
2024-05-15 20:01:35 +0200

    atoi done

---
+++ 0x05-C-Pointers_arrays_and_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "main.h"
+
+int _atoi(char *s)
+{
+    char *ptoc = s;
+    int result = 0, sign = 1;
+
+    while (*ptoc != '\0')
+    {
+        if (*ptoc == '-')
+        {
+            sign *= -1;
+        }
+        if (*ptoc >= '0' && *ptoc <= '9')
+        {
+            result = result * 10 + *ptoc - '0';
+            if (result > 0 && *(ptoc + 1) == ' ')
+            {
+                goto end;
+            }
+        }
+        ptoc++;
+    }
+end:
+    return result * sign;
+}
