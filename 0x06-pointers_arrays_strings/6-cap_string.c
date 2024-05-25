d9f26c3e9dd587f8317113c916f24bbca3a9d5d3
Anasselb <elboussailianass@gmail.com>
2024-05-25 21:51:50 +0200

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,20 +13,19 @@ char *cap_string(char *str)
 {
     int i = 0;
 
+    if (str[i] >= 'a' && str[i] <= 'z')
+        str[i] -= 32;
+
     while (str[i++] != '\0')
     {
-        if (str[i] == '\t')
-        {
-            str[i] = ' ';
-        }
 
-        if ((str[i] == ' ' || str[i] == '\n' || str[i] == ',' || str[i] == ';' ||
-            str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-            str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}') &&
-            (str[i + 1] != ' ' || str[i + 1] != '\n' || str[i + 1] != ',' ||
-            str[i + 1] != ';' || str[i + 1] != '.' || str[i + 1] != '!' ||
-            str[i + 1] != '?' || str[i + 1] != '"' || str[i + 1] != '(' ||
-            str[i + 1] != ')' || str[i + 1] != '{' || str[i + 1] != '}') &&
+        if ((str[i] == '\t' || str[i] == ' ' || str[i] == '\n' || str[i] == ',' || str[i] == ';' ||
+             str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+             str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}') &&
+            (str[i] != '\t' || str[i + 1] != ' ' || str[i + 1] != '\n' || str[i + 1] != ',' ||
+             str[i + 1] != ';' || str[i + 1] != '.' || str[i + 1] != '!' ||
+             str[i + 1] != '?' || str[i + 1] != '"' || str[i + 1] != '(' ||
+             str[i + 1] != ')' || str[i + 1] != '{' || str[i + 1] != '}') &&
             str[i + 1] >= 'a' && str[i + 1] <= 'z')
         {
             str[i + 1] -= 32;
